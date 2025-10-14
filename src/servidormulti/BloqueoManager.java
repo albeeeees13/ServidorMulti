8f8dd17f030a320390577e114dde2d786e8d8057
KEVIN ALBESTRAIN <albes@MacBookAir.mshome.net>
2025-10-14 07:36:53 -0700

    agregue la opcion para no poder bloquearme a mi mismo y guardar bloqueos

--- src/servidormulti/BloqueoManager.java
+++ src/servidormulti/BloqueoManager.java
@@ -7,3 +7,20 @@ public class BloqueoManager {
     static {
         cargarBloqueos();
     }
+    public static synchronized void bloquear(String quien, String aQuien) throws IOException {
+        if (quien.equalsIgnoreCase(aQuien)) {
+            throw new IOException("No puedes bloquearte a ti mismo.");
+        }
+
+        bloqueos.putIfAbsent(quien, new HashSet<>());
+        if (!ServidorMulti.clientes.containsKey(aQuien)) {
+            throw new IOException("El usuario '" + aQuien + "' no existe.");
+        }
+
+        if (bloqueos.get(quien).contains(aQuien)) {
+            throw new IOException("Ya tienes bloqueado a " + aQuien);
+        }
+
+        bloqueos.get(quien).add(aQuien);
+        guardarBloqueos();
+    }
