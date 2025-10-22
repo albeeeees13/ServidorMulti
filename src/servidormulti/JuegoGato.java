3046a4ed88a4b60dcd1144832ca8945e15725bb5
KEVIN ALBESTRAIN <albes@MacBook-Air-de-KEVIN.local>
2025-10-22 07:09:57 -0700

    hice el constructor que determina el primer turno aleatoriamente

--- src/servidormulti/JuegoGato.java
+++ src/servidormulti/JuegoGato.java
@@ -8,3 +8,18 @@ public class JuegoGato {
     public final String jugadorO;
     private String turnoActual;
     private final char[] tablero;
+
+    public JuegoGato(String retador, String retado) {
+        this.tablero = new char[9];
+        Arrays.fill(this.tablero, ' ');
+
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            this.jugadorX = retador;
+            this.jugadorO = retado;
+        } else {
+            this.jugadorX = retado;
+            this.jugadorO = retador;
+        }
+        this.turnoActual = this.jugadorX;
+    }
